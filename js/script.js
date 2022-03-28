bae8410b364c8e7c16bbf6f12c5a57c4933d2f52
Mirsoip <mirsoip.adilov@gmail.com>
2022-03-28 15:11:38 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// "use strict";
+// console.log("Hello world");
+
+let number = 10;
+const leftBorderWidth = 10;
+console.log(number);
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+const obj2 = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj2.name);
+// console.log(obj2['name']);
+
+let arr = ['plum.jpg', 'orange.png', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = prompt("Are you 18?", "Yes");
+console.log(answer);
+
